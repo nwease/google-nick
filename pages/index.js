841dc789fa10223576605e3e9af84290f82ac233
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2021-04-08 11:28:22 +0200

    Trying to figure out tailwind styles

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import {SearchIcon} from '@heroicons/react/outline';
 
 export default function Home() {
     return (
-        <div className=''>
+        <div className='h-screen'>
             <Head>
                 <title>
                     Google Nick
@@ -16,13 +16,14 @@ export default function Home() {
             {/*HEADER*/}
 
             {/*BODY*/}
-            <div className='w-full flex flex-col justify-center'>
+            <div className='flex flex-col max-w-xl m-auto h-full'>
                 <img
-                    className='object-contain h-20' src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
-                     alt='Google'
+                    className='object-contain h-20'
+                    src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+                    alt='Google'
                 />
 
-                <div className='flex rounded-full border border-gray-200 px-5 py-3 items-center hover:shadow-lg'>
+                <div className='flex rounded-full border border-gray-200 px-5 py-3 items-center shadow-sm'>
                     <SearchIcon className='h-5 mr-3 text-gray-500' />
 
                     <input className='flex-grow focus:outline-none' type='text'/>
@@ -31,11 +32,11 @@ export default function Home() {
                 </div>
 
                 <div className='flex justify-center mt-8 space-x-4'>
-                    <button className='bg-[#f8f9fa] p-3 rounded-md'>
+                    <button className='btn'>
                         Google Search
                     </button>
 
-                    <button className='bg-[#f8f9fa] p-3 rounded-md'>
+                    <button className='btn'>
                         I'm Feeling Lucky
                     </button>
                 </div>
