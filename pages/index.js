0a3902b04099461f5ed7de8c930a656de8801eb9
Nicholas Wease <nicholaswease@Nicholass-MacBook-Air.local>
2021-04-09 11:17:35 +0200

    modified alot of stuff

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Head from 'next/head'
 import {MicrophoneIcon} from '@heroicons/react/solid';
 import {SearchIcon} from '@heroicons/react/outline';
+import Link from 'next/link';
 
 export default function Home() {
     return (
-        <div className='h-screen'>
+        <div className='flex flex-col items-center justify-center h-screen'>
             <Head>
                 <title>
                     Google Nick
@@ -14,16 +15,50 @@ export default function Home() {
             </Head>
 
             {/*HEADER*/}
+            <div className='flex w-full p-5 justify-between text-sm'>
+                <div className='flex gap-x-4 items-center'>
+                    <a href='/about' className='hover:underline'>
+                        About
+                    </a>
+
+                    <a href='/store' className='hover:underline'>
+                        Store
+                    </a>
+                </div>
+
+                <div className='flex gap-x-4 items-center'>
+                    <a href='/gmail' className='hover:underline'>
+                        Gmail 3
+                    </a>
+
+                    <a href='/images' className='hover:underline'>
+                        Images
+                    </a>
+
+                    <img className='h-8'
+                        src='https://lh3.googleusercontent.com/proxy/tYj3PAybaHfPcpEX3A51Z5tyhRmFHi2ov1YcAn3bOnrqQi8Xtdtl4Tbeyd6XQIK_KxBBQNQZjRRv0d-HtgdEouFH'
+                        alt='Me'
+                    />
+                </div>
+            </div>
 
             {/*BODY*/}
-            <div className='flex flex-col max-w-xl m-auto h-full'>
+            {/*<div>*/}
+            {/*    <img*/}
+            {/*        className='object-contain h-20'*/}
+            {/*        src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'*/}
+            {/*     alt='Google'*/}
+            {/*    />*/}
+            {/*</div>*/}
+
+            <div className='flex flex-1 items-center justify-center flex-col w-full'>
                 <img
                     className='object-contain h-20'
                     src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
                     alt='Google'
                 />
 
-                <div className='flex rounded-full border border-gray-200 px-5 py-3 items-center shadow-sm'>
+                <div className='flex w-full hover:shadow-lg focus-within:shadow-lg max-w-xl rounded-full border border-gray-200 px-5 py-3 items-center'>
                     <SearchIcon className='h-5 mr-3 text-gray-500' />
 
                     <input className='flex-grow focus:outline-none' type='text'/>
@@ -31,7 +66,7 @@ export default function Home() {
                     <MicrophoneIcon className='h-5' />
                 </div>
 
-                <div className='flex justify-center mt-8 space-x-4'>
+                <div className='flex flex-col w-1/2 gap-y-2 justify-center mt-8 sm:gap-y-0 sm:flex-row sm:gap-x-4'>
                     <button className='btn'>
                         Google Search
                     </button>
